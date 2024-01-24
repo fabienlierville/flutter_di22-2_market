a9b41f19c9c63309790075fe40edd12c46dbaa5b
fabien <flierville@cesi.fr>
2024-01-24 11:36:19 +0100

    GetALL

--- lib/repositories/magasin_repository.dart
+++ lib/repositories/magasin_repository.dart
@@ -14,4 +14,17 @@ class MagasinRepository{
     //REtourner le magasin ajouté (pour avoir l'ID)
     return magasin;
   }
+
+  /* GET ALL */
+  static Future<List<Magasin>> getAll() async{
+    // Récupérer la connexion BDD
+    Database bdd = await DataBaseService.database;
+    List<Map<String,dynamic>> result = await bdd.query("magasin");
+    // Parcourir la map pour retourner une liste d'objet
+    List<Magasin> l = [];
+    result.forEach((Map<String,dynamic> ligne) {
+      l.add(Magasin.fromMap(ligne));
+    });
+    return l;
+  }
 }
